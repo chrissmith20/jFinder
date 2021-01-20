a243d91b3b8d04e40ba2a6840df275906ae87bc2
Evan <evfernberg@gmail.com>
2021-01-20 14:40:07 -0500

    added restful api

--- app/javascript/react/components/JobContainer.js
+++ app/javascript/react/components/JobContainer.js
@@ -1,15 +1,42 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 import JobTile from "./JobTile"
 
 const JobContainer = (props) => {
+
+  const [ jobs, setJobs ] = useState([])
+
+  useEffect(() => {
+  fetch('/api/v1/jobs.json')
+    .then(response => {
+      if(response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+        throw error
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      setJobs(body)
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+}, []);
+
+  const jobTitles = jobs.map((x) => {
+    return(
+      <JobTile
+        key={x.id}
+        tileData={x}
+      />
+    )
+  });
+
   return(
     <div>
       <h1>AYYYYYYYYYYY</h1>
-
-       <JobTile
-
-       />
-
+      <h1>{jobTitles}</h1>
     </div>
   )
 };
