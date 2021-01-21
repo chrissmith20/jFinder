ca81fbbb635c8203d90f05d2896032c3e0a724eb
Evan <evfernberg@gmail.com>
2021-01-21 17:34:16 -0500

    Commented out unnecessary code/future features

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -5,38 +5,37 @@ require 'pry'
 class Scraper
 
   def scraper_method
-    binding.pry
     url = "https://www.indeed.com/jobs?q=Software%20Engineer&l=Boston%2C%20MA&rbl=Boston%2C%20MA&jlid=e167aeb8a259bcac&sort=date&vjk=04ef7a50c33007f7"
     page_content = HTTParty.get(url)
     parsed_content = Nokogiri::HTML(page_content)
 
     job_listings = parsed_content.css('div.jobsearch-SerpJobCard')
-    job_company = job_listings.css('span.company')
-    job_role = job_listings.css('h2.title')
-    job_location = job_listings.css('span.accessible-contrast-color-location')
-    job_salary = job_listings.css('span.salaryText')
+
+    # job_company = job_listings.css('span.company')
+    # job_role = job_listings.css('h2.title')
+    # job_location = job_listings.css('span.accessible-contrast-color-location')
+    # job_salary = job_listings.css('span.salaryText')
+
     # job_link = job_role.css('a')[0].attributes['href'].value
 
     #Hard code. Need to automate the following array:
-    job_urls = [
-      job_role.css('a')[0].attributes['href'].value,
-      job_role.css('a')[1].attributes['href'].value,
-      job_role.css('a')[2].attributes['href'].value,
-      job_role.css('a')[3].attributes['href'].value,
-      job_role.css('a')[4].attributes['href'].value,
-      job_role.css('a')[5].attributes['href'].value,
-      job_role.css('a')[6].attributes['href'].value,
-      job_role.css('a')[7].attributes['href'].value,
-      job_role.css('a')[8].attributes['href'].value,
-      job_role.css('a')[9].attributes['href'].value,
-      job_role.css('a')[10].attributes['href'].value,
-      job_role.css('a')[11].attributes['href'].value,
-      job_role.css('a')[12].attributes['href'].value,
-      job_role.css('a')[13].attributes['href'].value,
-      job_role.css('a')[14].attributes['href'].value
-    ]
-
-    job_b = parsed_content.css('tr')
+    # job_urls = [
+    #   job_role.css('a')[0].attributes['href'].value,
+    #   job_role.css('a')[1].attributes['href'].value,
+    #   job_role.css('a')[2].attributes['href'].value,
+    #   job_role.css('a')[3].attributes['href'].value,
+    #   job_role.css('a')[4].attributes['href'].value,
+    #   job_role.css('a')[5].attributes['href'].value,
+    #   job_role.css('a')[6].attributes['href'].value,
+    #   job_role.css('a')[7].attributes['href'].value,
+    #   job_role.css('a')[8].attributes['href'].value,
+    #   job_role.css('a')[9].attributes['href'].value,
+    #   job_role.css('a')[10].attributes['href'].value,
+    #   job_role.css('a')[11].attributes['href'].value,
+    #   job_role.css('a')[12].attributes['href'].value,
+    #   job_role.css('a')[13].attributes['href'].value,
+    #   job_role.css('a')[14].attributes['href'].value
+    # ]
 
     jobs = []
 
@@ -54,8 +53,8 @@ class Scraper
       jobs << job_info
     end
 
-
   end
 end
+
 scrape = Scraper.new
 scrape.scraper_method()
