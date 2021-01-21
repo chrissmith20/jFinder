a4ab3b370dcd5e50ee8a50d5b76fc5cb12a246ce
Evan <evfernberg@gmail.com>
2021-01-21 17:33:53 -0500

    Added function to create an object from data that is scraped

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,4 +8,11 @@ class Job < ApplicationRecord
   validates :description, presence: true
   validates :url, presence: true
 
+  def self.create_from_collection(jobs)
+    jobs.each do |hash_array|
+      hash_array.each do |hash|
+          Job.create(position: hash[:position], company: hash[:company], location: hash[:location], salary: hash[:salary], date: hash[:date], description: hash[:description], url: hash[:url])
+      end
+    end
+  end
 end
